4c67042035149783d9e1dd2780fb892425fea0f5
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-07-26 11:29:33 -0500

    showPage(list,page)

--- js/script.js
+++ js/script.js
@@ -18,6 +18,43 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list,page){
+  
+   // the start index and the end index of the list items to be displayed on the given page
+   const start_Index = (page*9)-9;
+   const end_Index = (page*9);
+
+   const ul = document.getElementsByClassName("student-list");
+   const student_list =ul[0];
+   var HTML =``;
+
+ 
+   for (let i = start_Index; i < end_Index ; i++ ){
+
+      HTML +=` <li class="student-item cf">
+      <div class="student-details">
+        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+        <h3>${list[i].name.first} ${list[i].name.last}</h3>
+        <span class="email">${list[i].email}</span>
+      </div>
+      <div class="joined-details">
+        <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+    </li>`;
+
+   }//end for
+
+   console.log("html string",HTML);
+   console.log("end index",end_Index);
+
+   student_list.insertAdjacentHTML('beforeend',HTML);
+   // element.insertAdjacentHTML(position, text);
+   // 'beforeend' : Juste à l'intérieur de l'element , après son dernier enfant.
+
+}//end showPage function
+
+showPage(data,3);
+
 
 
 /*
